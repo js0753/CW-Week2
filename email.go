5a8a67cd44d4ef23236aac9eedbda7220359e2f7
Harshavardhan <taleleharshavardhan1@gmail.com>
2021-06-06 22:10:09 +0530

    gomail added as dependency, email.go implemented

---
+++ email.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	gomail "gopkg.in/gomail.v2"
+)
+
+func sendMail(from string, password string, to string) {
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", from)
+	mailer.SetHeader("To", to)
+	mailer.SetHeader("Subject", "Mail from Go")
+	mailer.SetBody("text/plain", "PFA")
+	mailer.Attach("./exported.json")
+	dialer := gomail.NewDialer("smtp.gmail.com", 587, from, password)
+	if err := dialer.DialAndSend(mailer); err != nil {
+		fmt.Println(err)
+		log.Fatalln("Error occured during sendig mail:", err)
+	} else {
+		fmt.Println("Mail sent successfully")
+	}
+}
--- main.go
+++ main.go
@@ -3,4 +3,5 @@ package main
 func main() {
 	jtt := readJsonData("jiraTestTeam.json")
 	jsonFromObj(jtt)
+	sendMail("dev.js0753@gmail.com", "devgmail55", "dev.js0753@gmail.com")
 }
