d7b1f87410b9363d6659e963494ea3d06bfb664f
js0753 <jainam02@somaiya.edu>
2021-06-09 16:57:27 +0530

    mailx comand

--- email.go
+++ email.go
@@ -1,24 +1,37 @@
 package main
 
+
+
+
 import (
 	"fmt"
-	"log"
+	"os/exec"
+	// "log"
 
-	gomail "gopkg.in/gomail.v2"
+	// gomail "gopkg.in/gomail.v2"
 )
 
-func sendMail(from string, password string, to string) {
-	mailer := gomail.NewMessage()
-	mailer.SetHeader("From", from)
-	mailer.SetHeader("To", to)
-	mailer.SetHeader("Subject", "Mail from Go")
-	mailer.SetBody("text/plain", "PFA")
-	mailer.Attach("./exported.json")
-	dialer := gomail.NewDialer("smtp.gmail.com", 587, from, password)
-	if err := dialer.DialAndSend(mailer); err != nil {
+// func sendMail(from string, password string, to string) {
+// 	mailer := gomail.NewMessage()
+// 	mailer.SetHeader("From", from)
+// 	mailer.SetHeader("To", to)
+// 	mailer.SetHeader("Subject", "Mail from Go")
+// 	mailer.SetBody("text/plain", "PFA")
+// 	mailer.Attach("./exported.json")
+// 	dialer := gomail.NewDialer("smtp.gmail.com", 587, from, password)
+// 	if err := dialer.DialAndSend(mailer); err != nil {
+// 		fmt.Println(err)
+// 		log.Fatalln("Error occured during sendig mail:", err)
+// 	} else {
+// 		fmt.Println("Mail sent successfully")
+// 	}
+// }
+
+func sendMail(recipient string){
+//command = " mailx -s \"Exported Json File\" -A exported.json  "+recipient
+cmd := exec.Command("mailx","-s","\"Exported Json File\"","-A","exported.json",recipient)
+	err := cmd.Run()
+	if err != nil {
 		fmt.Println(err)
-		log.Fatalln("Error occured during sendig mail:", err)
-	} else {
-		fmt.Println("Mail sent successfully")
 	}
 }
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 func main() {
 	jtt := readJsonData("jiraTestTeam.json")
 	jsonFromObj(jtt)
-	sendMail("dev.js0753@gmail.com", "devgmail55", "dev.js0753@gmail.com")
+	sendMail("dev.js0753@gmail.com")
 }
